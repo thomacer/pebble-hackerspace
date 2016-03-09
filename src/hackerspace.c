8e5870e198f1a12189aab2d0a26fa3d8d08a1030
Perale Thomas <thomas.perale@openmailbox.org>
2016-03-09 17:09:17 +0100

    Auto resize for bitmaps.

--- src/hackerspace.c
+++ src/hackerspace.c
@@ -12,7 +12,7 @@
 #define KEY_LIST_OF_PEOPLE_PRESENT 7
 
 static char* space_info_title[NUMBER_OF_SCRAPPED_INFO];
-static char* space_info_subtitle[NUMBER_OF_SCRAPPED_INFO];
+/* static char* space_info_subtitle[NUMBER_OF_SCRAPPED_INFO]; */
 /* static char** space_info_icons[NUMBER_OF_SCRAPPED_INFO]; */
 
 /* This variable will be set when we get the JSON from SpaceAPI. */
@@ -122,6 +122,26 @@ static int16_t menu_get_header_height_callback(MenuLayer* menu_layer, uint16_t s
     return MENU_CELL_BASIC_HEADER_HEIGHT;
 }
 
+/* @desc Get cell size.
+ */
+static int16_t menu_get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
+    if (menu_layer_is_index_selected(menu_layer, cell_index)) {
+        switch (cell_index->section) {
+            case 0:
+                switch (cell_index->row) {
+                    case 0:
+                        return gbitmap_get_bounds(s_logo_bitmap).size.h;
+                    default:
+                        return MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT;
+                }
+            default:
+                return MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT;
+        }
+    } else {
+        return MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT;
+    }
+}
+
 /* @desc Draw the headers sections.
  *
  * @param {ctx} :
@@ -151,7 +171,8 @@ static void menu_draw_row_callback(GContext* ctx, const Layer* cell_layer, MenuI
         case 0: ;
             GSize size = layer_get_frame(cell_layer).size;
             const uint8_t x_offset = (size.w - gbitmap_get_bounds(s_logo_bitmap).size.w) / 2;
-            graphics_draw_bitmap_in_rect(ctx, s_logo_bitmap, GRect(x_offset, 0, size.w, size.h));
+            const uint8_t y_offset = (size.h - gbitmap_get_bounds(s_logo_bitmap).size.h) / 2;
+            graphics_draw_bitmap_in_rect(ctx, s_logo_bitmap, GRect(x_offset, y_offset, size.w, size.h));
             break;
         case 1:
             menu_cell_basic_draw(ctx, cell_layer, space_info_title[cell_index->row], NULL, NULL);
@@ -212,7 +233,7 @@ static void window_load(Window *window) {
     .draw_header = menu_draw_header_callback,
     .draw_row = menu_draw_row_callback,
     .select_click = menu_select_callback,
-    .get_cell_height = NULL,
+    .get_cell_height = menu_get_cell_height_callback,
   });
 
   // Bind the menu layer's click config provider to the window for interactivity
