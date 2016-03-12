35ba5e31acf1238e0a9f3e060c2090de1074eec7
Perale Thomas <thomas.perale@openmailbox.org>
2016-03-12 17:37:52 +0100

    Added contact window.

---
+++ src/windows/win_contact.c
@@ -0,0 +1,104 @@
+#include "./win_contact.h"
+#include "../libs/pebble-assist.h"
+
+static Window* window = NULL;
+
+static SimpleMenuLayer* s_menu_layer = NULL;
+static GBitmap* s_header_bitmap = NULL;
+
+static uint16_t space_contact_current_number = 0;
+
+#define NUMBER_OF_SECTIONS 1
+static SimpleMenuSection s_menu_sections[NUMBER_OF_SECTIONS];
+#define NUMBER_OF_CONTACT_ITEMS 4
+static SimpleMenuItem s_contact_menu_items[NUMBER_OF_CONTACT_ITEMS];
+
+
+static void action_callback (int index, void *ctx) {
+
+}
+
+
+/* ------------------------------------------------------------------------
+ *                      Partie création des layers.
+ * ------------------------------------------------------------------------
+ */
+static void window_load (Window *window) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Launching window loading.");
+
+  space_contact_current_number = 0;
+
+  if (t_email) {
+    s_contact_menu_items[space_contact_current_number++] = (SimpleMenuItem) {
+        .title = "Mail",
+        .subtitle = t_email->value->cstring,
+        .callback = action_callback,
+    };
+    DEBUG("Added mail : %i", space_contact_current_number);
+  }
+
+  if (t_irc) {
+    s_contact_menu_items[space_contact_current_number++] = (SimpleMenuItem) {
+        .title = "IRC",
+        .subtitle = t_irc->value->cstring,
+        .callback = action_callback,
+    };
+    DEBUG("Added irc : %i", space_contact_current_number);
+  }
+
+  if (t_facebook) {
+    s_contact_menu_items[space_contact_current_number++] = (SimpleMenuItem) {
+        .title = "Facebook",
+        .subtitle = t_facebook->value->cstring,
+        .callback = action_callback,
+    };
+    DEBUG("Added facebook : %i", space_contact_current_number);
+  }
+
+  if (t_twitter) {
+    s_contact_menu_items[space_contact_current_number++] = (SimpleMenuItem) {
+        .title = "Twitter",
+        .subtitle = t_twitter->value->cstring,
+        .callback = action_callback,
+    };
+    DEBUG("Added twitter in position : %i", space_contact_current_number);
+  }
+
+  s_menu_sections[0] = (SimpleMenuSection) {
+      .title = "Contacts section",
+      .num_items = space_contact_current_number,
+      .items = s_contact_menu_items,
+  };
+
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_frame(window_layer);
+
+  s_menu_layer = simple_menu_layer_create(bounds, window, s_menu_sections, NUMBER_OF_SECTIONS, NULL);
+
+  layer_add_child(window_layer, simple_menu_layer_get_layer(s_menu_layer));
+
+  DEBUG("Finished doing the menu layer for the contact window.");
+}
+
+static void window_unload(Window *window) {
+  simple_menu_layer_destroy(s_menu_layer);
+}
+
+void win_contact_show(void) {
+  window_stack_push(window, true);
+}
+
+void win_contact_init(void) {
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "CREATING NEW WINDOW : contact_window");
+  window = window_create();
+
+  window_set_window_handlers(window, (WindowHandlers) {
+    .load = window_load,
+    .unload = window_unload,
+  });
+}
+
+void win_contact_deinit(void) {
+    window_unload(window);
+    window_destroy(window);
+}
---
+++ src/windows/win_contact.h
@@ -0,0 +1,9 @@
+#ifndef _CONTACT_WINDOW_H
+#define _CONTACT_WINDOW_H
+#include <pebble.h>
+#include "../globals.h"
+
+void win_contact_init(void);
+void win_contact_deinit(void);
+
+#endif
