6b35387907c9747495e9284a3b91ba463beb276d
Perale Thomas <thomas.perale@openmailbox.org>
2016-06-05 21:18:16 +0200

    Added Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+  var appConfig = {
+      info: grunt.file.readJSON('appinfo.json')
+  };
+
+  grunt.initConfig({
+        jshint : {
+            files : [
+                'Gruntfile.js',
+                'src/js/**/*.js',
+                'config/js/dev/**/*.js',
+                '!src/js/**/*.tpl.js',
+                '!src/js/pebble-js-app.js',
+            ],
+            options : {
+                jshintrc : true, 
+            }
+        },
+
+        config: appConfig, 
+
+        copy: {
+            main: {
+                options: {
+                    process: function (content, path) {
+                        return grunt.template.process(content);
+                    }
+                },
+                files: {
+                    'src/appinfo.h': ['src/appinfo.tpl.h'],
+                    'src/js/appinfo.js': ['src/js/appinfo.tpl.js'],
+                }
+            },
+        },
+
+        browserify: {
+            dist: {
+                options: {
+                    transform: [
+                        ['babelify', {
+                            presets: ['es2015'],
+                        }]
+                    ]
+                },
+
+                files: {                                                         
+                    './src/js/pebble-js-app.js' : ['src/js/**/*.js', '!src/js/**/*.tpl.js', '!src/js/pebble-js-app.js'],
+                    './config/js/obs.js' : ['./config/js/dev/**/*'],
+                },
+            },
+        },
+  });
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('default', ['copy', 'jshint', 'browserify']);
+};
