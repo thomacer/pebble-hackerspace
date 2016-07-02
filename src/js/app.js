96923792cc29d7a95e1513461684e96bbad2086e
Perale Thomas <thomas.perale@openmailbox.org>
2016-07-02 18:48:30 +0200

    App modified to use other classes.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,11 @@
-var SPACE_INFO_TYPE = 0;
-var PEOPLE_PRESENT_LIST_ELEMENT_TYPE = 1;
+const Contact = require('contact.js');
+const Sensors = require('sensors.js');
+const utils = require('utils.js');
+const async = require('async');
+const app = require('appinfo.js')
 
-var xhrRequest = function (url, type, callback) {
-  var xhr = new XMLHttpRequest();
+const xhrRequest = function (url, type, callback) {
+  const xhr = new XMLHttpRequest();
   xhr.onload = function () {
       callback(this.responseText);
     };
@@ -10,93 +13,36 @@ var xhrRequest = function (url, type, callback) {
   xhr.send();
 };
 
+const getAPIResult = function () {
+    const url = 'http://urlab.be/spaceapi.json';
 
-/* @desc : Send a preformated list item to pebble c.
- *
- * @param {item} : Item from a list.
- * @param {index} : Index of the item.
- */
-var sendListItem = function (item, index) {
-    // Send the message
-    Pebble.sendAppMessage(item, function () {
-        console.log("SENT (list) : " + JSON.stringify(dict));
-    }, function () {
-        console.log('Item transmission failed at index ' + index + ' for : ' + items);
-    });
-}
-
-/* @desc : Send the list of people present in the space to the pebble.
- *
- * @param {list} : List of people present.
- */
-var send_people_present_list = function (list) {
-    if (!list) {
-        console.log("No people info to send.");
-        return; 
-    }
-
-    var index = 0;
-
-    for (var index = 0; index < list.length; ++i) {
-        // Build message
-        var item = {
-            "KEY_ELEMENT" : list[index],
-            "KEY_INDEX" : index,
-            "KEY_SIZE" : list.length,
-            "KEY_TYPE" : 1,
-        };
-
-        sendListItem(item, index);
-    }
-}
-
-var getAPIResult = function () {
-    var url = "http://urlab.be/spaceapi.json";
-
-    console.log("Requesting " + url);
+    console.log('Requesting ' + url);
 
     xhrRequest(url, 'GET', 
         function(responseText) {
-            var json = JSON.parse(responseText);
+            const json = JSON.parse(responseText);
 
-            var result_dict = {
-                "KEY_SPACE" : json['space'],
-                "KEY_SPACE_URL" : json['url'],
-
-                // ---- Contact section information.               
-                "KEY_CONTACT_PHONE_NUMBER" : json['contact']['phone'],
-                "KEY_CONTACT_SIP_ADDRESS" : json['contact']['sip'],
-                "KEY_CONTACT_IRC" : json['contact']['irc'],
-                "KEY_CONTACT_TWITTER" : json['contact']['twitter'],
-                "KEY_CONTACT_FACEBOOK" : json['contact']['facebook'],
-                "KEY_CONTACT_IDENTICA" : json['contact']['identica'],
-                "KEY_CONTACT_FOURSQUARE" : json['contact']['foursquare'],
-                "KEY_CONTACT_EMAIL" : json['contact']['email'],
-                "KEY_CONTACT_MAILLING_LIST" : json['contact']['ml'],
-                "KEY_CONTACT_JABBER" : json['contact']['jabber'],
-                "KEY_CONTACT_ISSUE_MAIL" : json['contact']['issue_mail'],
-
-                "KEY_OPEN_STATE" : json['state']['open'],
-                "KEY_NUMBER_OF_PEOPLE_PRESENT" : json['sensors']['people_now_present'][0]['value'],
-
-                "KEY_TYPE" : 0,
-            };
-
-            console.log("SENDING : " + JSON.stringify(result_dict));
-
-            // Send to Pebble
-            Pebble.sendAppMessage(result_dict,
-                function (data) {
-                    send_people_present_list(json['sensors']['people_now_present'][0]['names']);
-                    console.log('API info sent to Pebble successfully!');
-                },
-                function (data, err) {
-                    console.log('Error sending API info to Pebble for ' + JSON.stringify(data) + ' : ' + err);
+            async.series([
+                (callback) => {
+                    utils.sendToPebble({
+                        'KEY_TYPE' : app.KEY_BASIC,
+                        'KEY_SPACE' : json['space'],
+                        'KEY_SPACE_URL' : json['url'],
+                        'KEY_OPEN_STATE' : json['state']['open'],
+                        'KEY_NUMBER_OF_PEOPLE_PRESENT' : json['sensors']['people_now_present'][0]['value'],
+                    }, callback);
+                }, (callback) => {
+                    let tmp = new Sensors(json['sensors']);
+                    tmp.send(callback);
+                }, (callback) => {
+                    let tmp = new Contact(json['contact']);
+                    tmp.send(callback);
                 }
-            );
+
+            ]);
         }
     );
-}
+};
 
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready', 
