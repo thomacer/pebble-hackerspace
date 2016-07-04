c50259b68792aede5d037e3e95982e275abefb1f
Perale Thomas <thomas.perale@openmailbox.org>
2016-07-05 00:40:48 +0200

    Fix communication with the C part for contact section.

--- src/js/contact.js
+++ src/js/contact.js
@@ -8,29 +8,33 @@ class Contact {
     send (callback) {
         const self = this;
 
-        const formatedObject = {
-            'KEY_TYPE' : app.KEY_CONTACT,
+        if (this.obj) {
+            const formatedObject = {
+                'KEY_TYPE' : app.KEY_CONTACT,
 
-            'KEY_CONTACT_PHONE_NUMBER' : self.obj['contact']['phone'],
-            'KEY_CONTACT_SIP_ADDRESS' : self.obj['contact']['sip'],
-            'KEY_CONTACT_IRC' : self.obj['contact']['irc'],
-            'KEY_CONTACT_TWITTER' : self.obj['contact']['twitter'],
-            'KEY_CONTACT_FACEBOOK' : self.obj['contact']['facebook'],
-            'KEY_CONTACT_IDENTICA' : self.obj['contact']['identica'],
-            'KEY_CONTACT_FOURSQUARE' : self.obj['contact']['foursquare'],
-            'KEY_CONTACT_EMAIL' : self.obj['contact']['email'],
-            'KEY_CONTACT_MAILLING_LIST' : self.obj['contact']['ml'],
-            'KEY_CONTACT_JABBER' : self.obj['contact']['jabber'],
-            'KEY_CONTACT_ISSUE_MAIL' : self.obj['contact']['issue_mail'],
-        };
+                'KEY_CONTACT_PHONE_NUMBER' : self.obj['phone'],
+                'KEY_CONTACT_SIP_ADDRESS' : self.obj['sip'],
+                'KEY_CONTACT_IRC' : self.obj['irc'],
+                'KEY_CONTACT_TWITTER' : self.obj['twitter'],
+                'KEY_CONTACT_FACEBOOK' : self.obj['facebook'],
+                'KEY_CONTACT_IDENTICA' : self.obj['identica'],
+                'KEY_CONTACT_FOURSQUARE' : self.obj['foursquare'],
+                'KEY_CONTACT_EMAIL' : self.obj['email'],
+                'KEY_CONTACT_MAILLING_LIST' : self.obj['ml'],
+                'KEY_CONTACT_JABBER' : self.obj['jabber'],
+                'KEY_CONTACT_ISSUE_MAIL' : self.obj['issue_mail'],
+            };
 
-        Pebble.sendAppMessage(formatedObject, () => {
-            console.log('SENT : ' + JSON.stringify(formatedObject));
-            if (callback) callback(null);
-        }, () => {
-            console.log('Contact transmission failed ' + JSON.stringify(formatedObject));
-            if (callback) callback('Contact transmission failed ' + JSON.stringify(formatedObject));
-        });
+            Pebble.sendAppMessage(formatedObject, () => {
+                console.log('SENT : ' + JSON.stringify(formatedObject));
+                if (callback) callback(null);
+            }, () => {
+                console.log('Contact transmission failed ' + JSON.stringify(formatedObject));
+                if (callback) callback('Contact transmission failed ' + JSON.stringify(formatedObject));
+            });
+        } else if (callback) {
+            callback(null);
+        }
     }
 }
 
