32b821589914eef8557223b695999e5f57eb0d22
Perale Thomas <thomas.perale@openmailbox.org>
2016-07-27 19:26:16 +0200

    Log lib

---
+++ src/libs/log.h
@@ -0,0 +1,17 @@
+// Shorthand App Logging
+
+#define DISABLE_LOGGING false
+
+#if DISABLE_LOGGING
+#define LOG(...)
+#define DEBUG(...)
+#define INFO(...)
+#define WARN(...)
+#define ERROR(...)
+#else
+#define LOG(...) app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define DEBUG(...) app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
+#define INFO(...) app_log(APP_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#define WARN(...) app_log(APP_LOG_LEVEL_WARNING, __FILE__, __LINE__, __VA_ARGS__)
+#define ERROR(...) app_log(APP_LOG_LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+#endif
