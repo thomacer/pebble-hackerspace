8bac2f22d8dc21d870ff1af828261dd243bbf12e
Perale Thomas <thomas.perale@openmailbox.org>
2016-07-31 16:10:37 +0200

    Useless function possible mem leaks.

--- src/windows/win_sensor.c
+++ src/windows/win_sensor.c
@@ -4,20 +4,10 @@ static Window* s_window = NULL;
 
 static void* current_sensor_struct = NULL;
 
-/* Those two functions are empty because a sensor window depend on the sensor
- * you select to show so there is no real design to be constructed at the
- * load time. The design will be made at when the window is shown.
- */
-static void window_load (Window* window) {}
-static void window_unload (Window* window) {}
-
 static void window_appear (Window* window) {
   ((Sensor*) current_sensor_struct)->win_draw(window, current_sensor_struct);
 }
 
-/* @note : Can be problematic when the window disappear but it's still
- *      in the stack.
- */
 static void window_disappear (Window* window) {
   ((Sensor*) current_sensor_struct)->win_destroy(current_sensor_struct);
   current_sensor_struct = NULL;
@@ -36,11 +26,8 @@ void win_sensor_init (void) {
   s_window = window_create();
 
   window_set_window_handlers(s_window, (WindowHandlers) {
-    /* .load = window_load, */
-    .load = window_load,
     .appear = window_appear,
     .disappear = window_disappear,
-    .unload = window_unload,
   });
 }
 
