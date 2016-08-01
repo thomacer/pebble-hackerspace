d338c68771083d68f293ac18d58fcd199f05149b
Perale Thomas <thomas.perale@openmailbox.org>
2016-08-01 12:42:47 +0200

    Labels for mem states.

--- src/libs/log.h
+++ src/libs/log.h
@@ -10,12 +10,12 @@
 #define INFO(...)
 #define WARN(...)
 #define ERROR(...)
-#define MEM_STATE(info)
+#define MEM_STATE(label)
 #else
 #define LOG(...) app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define DEBUG(...) app_log(APP_LOG_LEVEL_DEBUG, __FILE__, __LINE__, __VA_ARGS__)
 #define INFO(...) app_log(APP_LOG_LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__)
 #define WARN(...) app_log(APP_LOG_LEVEL_WARNING, __FILE__, __LINE__, __VA_ARGS__)
 #define ERROR(...) app_log(APP_LOG_LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__)
-#define MEM_STATE() DEBUG("%d bytes free and %d bytes used.", heap_bytes_free(), heap_bytes_used())
+#define MEM_STATE(label) DEBUG("%s (%d bytes FREE and %d bytes USED)", label, heap_bytes_free(), heap_bytes_used())
 #endif
