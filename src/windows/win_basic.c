51fbfb1c83798d27167153e9d5be66d5d2570ef5
Perale Thomas <thomas.perale@openmailbox.org>
2016-08-16 21:31:50 +0200

    Converting timestamp to date.

--- src/windows/win_basic.c
+++ src/windows/win_basic.c
@@ -4,11 +4,11 @@ BasicInfo* basic_info = NULL;
 
 static Window* window;
 
+static TextLayer* last_change_info_text_layer = NULL;
 static TextLayer* last_change_text_layer = NULL;
 static char last_change_buffer[64];
 
 static TextLayer* state_text_layer = NULL;
-static char state_buffer[32];
 
 static void window_appear (Window* window) {
   Layer *window_layer = window_get_root_layer(window);
@@ -18,24 +18,32 @@ static void window_appear (Window* window) {
     return;
   }
 
-  state_text_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h / 2));
+  int16_t y_offset = 0;
+
+  state_text_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h / 4));
   if (basic_info->state == Closed) {
-    snprintf(state_buffer, 32, "Your space is closed.");
+    text_layer_set_text(state_text_layer, "Your space is closed.\n\n");
   } else if (basic_info->state == Open) {
-    snprintf(state_buffer, 32, "Your space is open.");
+    text_layer_set_text(state_text_layer, "Your space is open.\n\n");
   } else {
-    snprintf(state_buffer, 32, "Your space state is unknown.");
+    text_layer_set_text(state_text_layer, "Your space state is unknown.\n\n");
   }
 
-  text_layer_set_text(state_text_layer, state_buffer);
   text_layer_set_text_alignment(state_text_layer, GTextAlignmentCenter);
+  y_offset += text_layer_get_content_size(state_text_layer).h;
   layer_add_child(window_layer, text_layer_get_layer(state_text_layer));
 
   if (basic_info->lastchange) {
-    last_change_text_layer = text_layer_create(GRect(0, bounds.size.h / 2, bounds.size.w, bounds.size.h / 2));
-    snprintf(last_change_buffer, 64, "The space state last changed : %ld", basic_info->lastchange);
+    last_change_info_text_layer = text_layer_create(GRect(0, y_offset, bounds.size.w, bounds.size.h / 4));
+    text_layer_set_text(last_change_info_text_layer, "The space state changed on : ");
+    y_offset += text_layer_get_content_size(last_change_info_text_layer).h;
+    layer_add_child(window_layer, text_layer_get_layer(last_change_info_text_layer));
+
+    last_change_text_layer = text_layer_create(GRect(0, y_offset, bounds.size.w, bounds.size.h / 4));
+    strftime(last_change_buffer, 64, "%m/%d %H:%M:%S", localtime((time_t*) &(basic_info->lastchange)));
     text_layer_set_text(last_change_text_layer, last_change_buffer);
     text_layer_set_text_alignment(last_change_text_layer, GTextAlignmentCenter);
+    y_offset += text_layer_get_content_size(last_change_text_layer).h;
     layer_add_child(window_layer, text_layer_get_layer(last_change_text_layer));
   }
 }
@@ -46,9 +54,11 @@ static void window_disappear (Window* window) {
     state_text_layer = NULL;
   }
 
-  if (last_change_text_layer) {
-    text_layer_destroy(last_change_text_layer);
-    last_change_text_layer = NULL;
+  if (last_change_info_text_layer && last_change_text_layer) {
+    text_layer_destroy(last_change_info_text_layer );
+    last_change_info_text_layer = NULL;
+    text_layer_destroy(state_text_layer);
+    state_text_layer = NULL;
   }
 }
 
